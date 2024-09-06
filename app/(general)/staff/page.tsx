216b22b5011e50d8377f5886df80d76a65df26f3
Bandile Danxa <barnez76@gmail.com>
2024-09-06 03:48:24 +0200

    updated staff

--- app/(general)/staff/page.tsx
+++ app/(general)/staff/page.tsx
@@ -7,37 +7,23 @@ export default function Staff() {
             <div>
              <Image src="/staff-reduced.jpg" width={1910} height={700} priority={true} alt="banner-image"/>
             </div>
-            <div className="staff-wrapper mt-12 w-4/5 m-auto">
+            <div className="staff-wrapper mt-12 w-5/6 sm:w-4/5 m-auto">
                 <h1 className="mt-12 text-3xl text-center">Meet our academic staff</h1>
                 <div className="staff-photos-container mt-12">
-                    <div className="cards-wrapper flex justify-between mt-12">
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
+                    <div className="cards-wrapper sm:flex sm:flex-wrap sm:justify-between mt-6">
+                        <div className="staff-card mt-6">
+                            <Image src="/staff/staff-headshot.jpg" width={340} height={300} className="teacher-img" alt="staff-photo" />
+                            <p>Ms. C. Magerman</p>
                         </div>
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
+                        <div className="staff-card mt-6">
+                            <Image src="/staff/staff-headshot.jpg" width={340} height={300} className="teacher-img" alt="staff-photo" />
+                            <p>Ms. C. Magerman</p>
                         </div>
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
+                        <div className="staff-card mt-6">
+                            <Image src="/staff/staff-headshot.jpg" width={340} height={300} className="teacher-img" alt="staff-photo" />
+                            <p>Ms. C. Magerman</p>
                         </div>
                     </div>
-                    {/* <div className="cards-wrapper flex justify-between mt-12">
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
-                        </div>
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
-                        </div>
-                        <div className="staff-card">
-                            <Image src="/staff-photo.jpg" width={300} height={300} className="teacher-img" alt="staff-photo" />
-                            <p>Staff Name</p>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
